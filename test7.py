7b30d5cfb20bce92b3fe7d38164accfea7e1ec30
UmeshChandra123 <72588179+UmeshChandra123@users.noreply.github.com>
2020-10-10 10:45:42 +0530

    Create test7.py

---
+++ test7.py
@@ -0,0 +1,9 @@
+a = int(input("Enter value"))
+b = int(input("Enter value"))
+c = int(input("Enter value"))
+d = int(input("Enter value"))
+e = int(input("Enter value"))
+f = int(input("Enter value"))
+g = int(input("Enter value"))
+h = a+b+C+D+e+F+g
+print(H)
